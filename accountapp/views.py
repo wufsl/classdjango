f827a0d07da902f9757e1f336bff2696e1d19a1c
wufsl <jjourney@korea.ac.kr>
2023-08-23 05:36:08 +0900

    django course 16 commit

--- accountapp/views.py
+++ accountapp/views.py
@@ -4,5 +4,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def hello_world(request):
-    return render(request, 'accountapp/hello_world.html')
+
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text': 'POST METHOD!!!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text': 'GET METHOD!!!'})
+
+
+# 요청을 받은 객체 안에서 method를 찾고 post일 경우 기존 render 방식 쓰는데 추가적으로 안에다가 context라는 데이터꾸러미 안에 text이름을 가진 POST METHOD라는 문장을 넣어서 리턴
+
 
